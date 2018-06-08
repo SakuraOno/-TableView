89f4e6e09a7b65c6f13cccd37efbe986275e94e0
SakuraOno <skr.o_jun11@icloud.com>
2018-06-08 10:30:23 +0900

    IntとStringの書き換えが手こずった

--- ６、TableView/ViewController.swift
+++ ６、TableView/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView:UITableView!
+    
+    let random = arc4random_uniform(100)
+    let data = [arc4random_uniform(100)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // デリゲート
+        tableView.dataSource = self
+        
     }
-
+    
+    //セルの数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //ここでセル生成の数が指定される
+        return 20
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // セル生成
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let num1: Int = Int(arc4random_uniform(100))
+        
+        let sNum1:String = String(num1)
+        
+        cell.textLabel?.text = sNum1
+        return cell
+    }
+    
 }
-
